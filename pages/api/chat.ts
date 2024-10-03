447b6a846984e4b06de22b6f8a1c3e55ead6fb03
Ram N Sangwan <care@skillpedia.ai>
2024-10-03 21:12:45 +0530

    Chroma

--- pages/api/chat.ts
+++ pages/api/chat.ts
@@ -1,10 +1,8 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import type { Document } from 'langchain/document';
 import { OpenAIEmbeddings } from "@langchain/openai";
-import { PineconeStore } from "@langchain/pinecone";
 import { makeChain } from '@/utils/makechain';
-import { pinecone } from '@/utils/pinecone-client';
-import { PINECONE_INDEX_NAME, PINECONE_NAME_SPACE } from '@/config/pinecone';
+import { COLLECTION_NAME } from '@/config/chroma';
+import { Chroma } from "@langchain/community/vectorstores/chroma";
 
 export default async function handler(
   req: NextApiRequest,
@@ -13,7 +11,6 @@ export default async function handler(
   const { question, history } = req.body;
 
   console.log('question', question);
-  console.log('history', history);
 
   //only accept post requests
   if (req.method !== 'POST') {
@@ -28,53 +25,24 @@ export default async function handler(
   const sanitizedQuestion = question.trim().replaceAll('\n', ' ');
 
   try {
-    const index = pinecone.Index(PINECONE_INDEX_NAME);
-
     /* create vectorstore*/
-    const vectorStore = await PineconeStore.fromExistingIndex(
+    const vectorStore = await Chroma.fromExistingCollection(
       new OpenAIEmbeddings({}),
       {
-        pineconeIndex: index,
-        textKey: 'text',
-        namespace: PINECONE_NAME_SPACE, //namespace comes from your config folder
+        collectionName: COLLECTION_NAME,
       },
     );
 
-    // Use a callback to get intermediate sources from the middle of the chain
-    let resolveWithDocuments: (value: Document[]) => void;
-    const documentPromise = new Promise<Document[]>((resolve) => {
-      resolveWithDocuments = resolve;
-    });
-    const retriever = vectorStore.asRetriever({
-      callbacks: [
-        {
-          handleRetrieverEnd(documents) {
-            resolveWithDocuments(documents);
-          },
-        },
-      ],
-    });
-
     //create chain
-    const chain = makeChain(retriever);
-
-    const pastMessages = history
-      .map((message: [string, string]) => {
-        return [`Human: ${message[0]}`, `Assistant: ${message[1]}`].join('\n');
-      })
-      .join('\n');
-    console.log(pastMessages);
-
+    const chain = makeChain(vectorStore);
     //Ask a question using chat history
-    const response = await chain.invoke({
+    const response = await chain.call({
       question: sanitizedQuestion,
-      chat_history: pastMessages,
+      chat_history: history || [],
     });
-
-    const sourceDocuments = await documentPromise;
 
     console.log('response', response);
-    res.status(200).json({ text: response, sourceDocuments });
+    res.status(200).json(response);
   } catch (error: any) {
     console.log('error', error);
     res.status(500).json({ error: error.message || 'Something went wrong' });
